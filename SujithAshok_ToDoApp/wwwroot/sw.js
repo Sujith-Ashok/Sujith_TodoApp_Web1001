8773910c41db372eefd3f3bf5ac4d2af98a90250
Sujith Ashok <77700637+Sujith-Ashok@users.noreply.github.com>
2021-04-23 02:12:41 -0400

    added sw & manifest. website is installable now

---
+++ SujithAshok_ToDoApp/wwwroot/sw.js
@@ -0,0 +1,44 @@
+﻿const TodoCache = 'Sujith To-Do website cache';
+const TodoSecondaryCache = 'Sujith web application cache second';
+let urlsToCache = [
+    '/',
+    '/css/site.css',
+    '/js/site.js'
+
+];
+
+self.addEventListener('install', function (event) {
+    console.info('Service worker Installed');
+
+    event.waitUntil(
+        caches.open(TodoCache)
+            .then(function (cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+
+    console.info('the page is being cached');
+});
+
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function (response) {
+
+                if (response) {
+                    return response;
+                }
+
+                const newCache = fetch(event.request);
+                caches.open(TodoSecondaryCache).then((cache) => {
+                    cache.add(event.request);
+                });
+
+                return newCache;
+                //return fetch(event.request);
+            }
+            )
+    );
+});
