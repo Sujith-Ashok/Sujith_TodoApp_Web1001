beeca5ecefb0ce75ed1f1c5d311bbbae11a1657c
Sujith Ashok <77700637+Sujith-Ashok@users.noreply.github.com>
2021-04-22 23:13:25 -0400

    Completed part 1

--- SujithAshok_ToDoApp/Pages/Index.cshtml.cs
+++ SujithAshok_ToDoApp/Pages/Index.cshtml.cs
@@ -5,21 +5,41 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using SujithAshok_ToDoApp.Data;
 
 namespace SujithAshok_ToDoApp.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly DataContext _context;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, DataContext context)
         {
             _logger = logger;
+            _context = context;
         }
+
+        public DateTime example { get; set; }
+
+
+        [BindProperty]
+        public Models.Item IndexItem { get; set; }
+
+      
+
+        public ICollection<Models.Item> Completed;
+        public ICollection<Models.Item> Pending;
 
         public void OnGet()
         {
+            DateTime getDate = DateTime.Now;
+            DateTime TodayDate = getDate.Date;
+            DateTime TomorrowDate = TodayDate.AddDays(1);
 
+            Completed = _context.Items.Where(prod => prod.DoneDate == TodayDate).ToList();
+
+            Pending = _context.Items.Where(prod => prod.DueDate == TomorrowDate).ToList();
         }
     }
 }
--- SujithAshok_ToDoApp/Pages/Tasks.cshtml.cs
+++ SujithAshok_ToDoApp/Pages/Tasks.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using SujithAshok_ToDoApp.Models;
 
 namespace SujithAshok_ToDoApp.Pages
@@ -27,5 +28,54 @@ namespace SujithAshok_ToDoApp.Pages
         {
             TaskView = _context.Items.ToList();
         }
+
+        public void OnPost(int? ItemID)
+        {
+            Models.Item toDoComplete = _context.Items.FirstOrDefault(prod => prod.itemID == ItemID);
+            toDoComplete.Done = true;
+            _context.Update(toDoComplete);
+            _context.SaveChanges();
+
+            TaskView = _context.Items.ToList();
+
+        }
+
+        //public async Task<IActionResult> OnGetAsync(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    TaskItem = await _context.Items.FirstOrDefaultAsync(m => m.itemID == id);
+
+        //    if (TaskItem == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return Page();
+        //}
+
+
+        //public async Task<IActionResult> OnPostAsync(int? id)
+        //{
+        //    _context.Attach(Item).State = EntityState.Modified;
+
+        //    try
+        //    {
+        //        await _context.SaveChangesAsync();
+        //    }
+        //    catch (DbUpdateConcurrencyException)
+        //    {
+        //        if (!ItemExists(Item.itemID))
+        //        {
+        //            return NotFound();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
+        //}
     }
 }
