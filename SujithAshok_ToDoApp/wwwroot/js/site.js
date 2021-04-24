1f5237a1e977840f20f3b6becf5dccfbe6b5dd22
Sujith Ashok <77700637+Sujith-Ashok@users.noreply.github.com>
2021-04-23 23:52:09 -0400

    tried clipboard API and Final Commit

--- SujithAshok_ToDoApp/wwwroot/js/site.js
+++ SujithAshok_ToDoApp/wwwroot/js/site.js
@@ -118,40 +118,73 @@ window.addEventListener('offline', (event) => {
 
 });
 
-//    //if (navigator.onLine) {
-//    // $('#done').removeAttr('hidden')
-//    //}
 
+//(async function () {
+//    if ('clipboard' in window) {
+//        let canAsktoCopy = false;
+//        let canCopy = false;
+
+//        if ('permissions' in navigator) {
+//            let permState = await navigator.permissions.query({ name: 'clipboard' });
+//            if (permState.state !== 'denied') {
+//                canAsktoCopy = true;
+//                if (permState.state === 'granted') {
+//                    canCopy = true;
+//                }
+//            }
+//        } else {
+//            if (Notification.permission !== 'denied') {
+//                canAsktoCopy = true;
+//                if (Notification.permission === 'granted') {
+//                    canCopy = true;
+//                }
+//            }
+//        }
+
+//        if (canAsktoCopy && !canCopy) {
+//            $("#yesNotify").on("click", () => { clipboard.requestPermission() });
+//            $("#DontCopy").on("click", () => { $("#askForNotification").hide() });
+//            $('#sendNotifications').on("click", () => { $("#askForNotification").show() })
+//            $('#sendNotifications').removeAttr('hidden');
+//        }
+//    }
 //})()
 
+//var logTarget = document.getElementById('logTarget');
 
-    //(async function () {
-    //    if ('Notification' in window) {
-    //        let canAskNotify = false;
-    //        let canNotify = false;
+//function useAsyncApi() {
+//    return document.querySelector('input[value=async]').checked;
+//}
 
-    //        if ('permissions' in navigator) {
-    //            let permState = await navigator.permissions.query({ name: 'notifications' });
-    //            if (permState.state !== 'denied') {
-    //                canAskNotify = true;
-    //                if (permState.state === 'granted') {
-    //                    canNotify = true;
-    //                }
-    //            }
-    //        } else {
-    //            if (Notification.permission !== 'denied') {
-    //                canAskNotify = true;
-    //                if (Notification.permission === 'granted') {
-    //                    canNotify = true;
-    //                }
-    //            }
-    //        }
+//function log(event) {
+//    var timeBadge = new Date().toTimeString().split(' ')[0];
+//    var newInfo = document.createElement('p');
+//    newInfo.innerHTML = '' + timeBadge + ' ' + event + '.';
+//    logTarget.appendChild(newInfo);
+//}
 
-    //        if (canAskNotify && !canNotify) {
-    //            $("#yesNotify").on("click", () => { Notification.requestPermission() });
-    //            $("#noNotify").on("click", () => { $("#askForNotification").hide() });
-    //            $('#sendNotifications').on("click", () => { $("#askForNotification").show() })
-    //            $('#sendNotifications').removeAttr('hidden');
-    //        }
-    //    }
-    //})()
+//function performCopyEmail() {
+//    var selection = window.getSelection();
+//    var emailLink = document.querySelector('.js-emaillink');
+
+//    if (useAsyncApi()) {
+//        navigator.clipboard.writeText(emailLink.textContent)
+//            .then(() => log('Async writeText successful, "' + emailLink.textContent + '" written'))
+//            .catch(err => log('Async writeText failed with error: "' + err + '"'));
+//    } else {
+//        selection.removeAllRanges();
+//        var range = document.createRange();
+//        range.selectNode(emailLink);
+//        selection.addRange(range);
+
+//        try {
+//            var successful = document.execCommand('copy');
+//            var msg = successful ? 'successful' : 'unsuccessful';
+//            log('Copy email command was ' + msg);
+//        } catch (err) {
+//            log('execCommand Error', err);
+//        }
+
+//        selection.removeAllRanges();
+//    }
+//}
